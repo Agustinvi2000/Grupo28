9f71705f7e1e122c38450f2978dc663502ed4434
Agustinvi2000 <122182896+Agustinvi2000@users.noreply.github.com>
2024-05-11 01:14:33 -0300

    form contacto y favicon OK y retoques en todos los documentos.

--- public/js/contacto.js
+++ public/js/contacto.js
@@ -1,8 +1,35 @@
 const menu = document.querySelector(".menu");
-
-menu.addEventListener("click", function(){onOff()})
+const form = document.querySelector(".formulario");
+const boton = document.querySelector("button");
 
 function onOff(){
     const nav = document.querySelector(".header_nav");
     nav.classList.toggle("ocultar")
 }
+
+function validacion(){
+    const nombre = document.querySelector(".main1 input");
+    const apellido = document.querySelector(".main2 input");
+    const nacimiento = document.querySelector(".main7 input");
+    const email = document.querySelector(".main8 input");
+    const comentarios = document.querySelector(".main9 textarea");
+
+    const varon = document.querySelector(".main4 input");
+    const mujer = document.querySelector(".main5 input");
+    const otro = document.querySelector(".main6 input"); 
+
+    const radios = (varon.checked || mujer.checked || otro.checked)
+
+    if(nombre.value =='' || apellido.value =='' || nacimiento.value =='' || email.value =='' || comentarios.value =='' || radios == false){
+        alert("Todos los campos son obligatorios");
+        return false;
+    } 
+    return true;
+}
+
+menu.addEventListener("click", onOff);
+form.addEventListener("submit",(e) => {
+    if(!validacion()){
+        e.preventDefault();
+    }
+});
