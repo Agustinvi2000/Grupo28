dc757b150916da2def51bcd426885274dad9224f
Agustinvi2000 <122182896+Agustinvi2000@users.noreply.github.com>
2024-07-02 18:55:55 -0300

    Update galeria.js

--- src/public/js/galeria.js
+++ src/public/js/galeria.js
@@ -50,11 +50,11 @@ carouselComentar += `
             <div class="carousel-inner">
                 ${carouselInner}
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <button class="carousel-control prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <button class="carousel-control next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
@@ -113,4 +113,6 @@ document.querySelectorAll('.comentarButton').forEach(button => {
         }
     });
 });
+
+
 };
